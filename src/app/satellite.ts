21e0ff304e69fda902f84bf1109adae520e38fac
Vanessa Liu <user@Vanessas-MacBook-Pro.local>
2020-12-26 22:13:14 -0600

    updated satellite class and added orbit list app component

--- src/app/satellite.ts
+++ src/app/satellite.ts
@@ -1,2 +1,19 @@
 export class Satellite {
+    public name : string;
+    public type: string; 
+    public launchDate: string; 
+    public orbitType: string; 
+    public operational: boolean;
+
+    constructor(name: string, type: string, launchDate: string, orbitType: string, operational: boolean) {
+    this.name = name;
+    this.type = type
+    this.launchDate = launchDate 
+    this.orbitType = orbitType 
+    this.operational = operational
+    }
 }
+
+// export class Satellite {    // from sneppets.com
+//     constructor(public name: string, type: string, launchDate: string, orbitType: string, operational: boolean){  }
+// }
