61ca5b13e9bc0dcbf5b45501a5edb176da5d4da7
Vanessa Liu <user@Vanessas-MacBook-Pro.local>
2020-12-27 20:29:22 -0600

    six- Highlight Space Debris

--- src/app/satellite.ts
+++ src/app/satellite.ts
@@ -4,14 +4,26 @@ export class Satellite {
     public launchDate: string; 
     public orbitType: string; 
     public operational: boolean;
+    public alternateColor: string;
 
     constructor(name: string, type: string, launchDate: string, orbitType: string, operational: boolean) {
-    this.name = name;
+    this.name = name
     this.type = type
     this.launchDate = launchDate 
     this.orbitType = orbitType 
     this.operational = operational
     }
+
+    shouldShowWarning() { 
+
+        if(this.type.toUpperCase() === 'SPACE DEBRIS') {
+            return true;
+        } else {
+            return false;
+        }
+
+
+    } 
 }
 
 // export class Satellite {    // from sneppets.com
